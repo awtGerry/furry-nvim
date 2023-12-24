03b3f3f4af8d5d5a6c91286acbd759032c0e437e
Victor Rodriguez <awtgerry@gmail.com>
2023-12-23 23:22:11 -0600

    remove mason dep Switching to nix I saw that mason isn't doing the job.

--- lua/packer/plugins/lsp.lua
+++ lua/packer/plugins/lsp.lua
@@ -1,9 +1,13 @@
 return {
     "neovim/nvim-lspconfig",
     "onsails/lspkind-nvim",
+    "folke/neodev.nvim",
+    "folke/trouble.nvim",
+
+    -- For mason install
+    --[[
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    "folke/neodev.nvim",
-    "folke/trouble.nvim",
+    ]]
 }
